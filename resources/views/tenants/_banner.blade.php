9b7088ac151cc62348be536eac4efacdeb30aeef
CJ <vooncj@gmail.com>
2019-04-01 21:44:04 +0800

    finalized

--- resources/views/tenants/_banner.blade.php
+++ resources/views/tenants/_banner.blade.php
@@ -1,6 +1,6 @@
 
 <div class="banner">
-<span style="font-family:'Dancing Script'; font-size:20px">MoonWay Velocity Mall Map</span>
+<span style="font-family:'Dancing Script'; font-size:20px">MoonWay Velocity</span>
 @if(isset(Auth::user()->username))
     <button onclick="location.href='{{url('admin/logout')}}'"class="btn-primary" id="signOut">Sign out</button>
     <p style="float: right; margin-top: 5; margin-bottom: 0; padding-right: 20px">Welcome, <strong>{{ Auth::user()->username }}</strong></p>
